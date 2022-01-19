e25fbda022e2abf7c7e0168af7056ae85eb6ca96
Nom4D <arthur.adam@epitech.eu>
2022-01-19 17:44:14 +0100

    Aeris : mobile_client - Adding Architecture and ColorScheme

--- mobile/lib/src/main.dart
+++ mobile/lib/src/main.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// MaterialColor createMaterialColor(Color color) {
+//   List strengths = <double>[.05];
+//   final swatch = <int, Color>{};
+//   final int r = color.red, g = color.green, b = color.blue;
+//
+//   for (int i = 1; i < 10; i++) {
+//     strengths.add(0.1 * i);
+//   }
+//   strengths.forEach((strength) {
+//     final double ds = 0.5 - strength;
+//     swatch[(strength * 1000).round()] = Color.fromRGBO(
+//       r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+//       g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+//       b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+//       1,
+//     );
+//   });
+//   return MaterialColor(color.value, swatch);
+// }
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final ColorScheme aerisScheme = const ColorScheme(
+    primary: Color.fromRGBO(55, 71, 79, 1),
+    secondary: Color.fromRGBO(240, 98, 146, 1),
+    background: Colors.white,
+    brightness: Brightness.light,
+    onError: Colors.red,
+    error: Colors.red,
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
+    onBackground: Colors.black,
+    surface: Color(0xFF808080),
+    onSurface: Color(0xFF241E30),
+  );
 
   // This widget is the root of your application.
   @override
@@ -13,16 +47,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: aerisScheme
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,15 +56,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +68,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -109,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
