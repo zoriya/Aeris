dc3d5e9ed007b856a5cdabc1c38edcee4266be49
Arthi-chaud <arthur.jamet@gmail.com>
2022-01-22 10:11:16 +0100

    Mobile Client: create class for pipeline, to be completed

--- mobile/lib/src/models/pipeline.dart
+++ mobile/lib/src/models/pipeline.dart
@@ -1,6 +1,35 @@
+import 'package:flutter/material.dart';
+
 /// Object representation of a pipeline
 class Pipeline {
+  // TODO Is the data type of the id correct?
+  // Unique identifier
+  final int id;
+
   /// Name of the pipeline, defined by the user
-  String name;
+  final String name;
+
   /// How many times the pipeline was triggered
+  final int triggerCount;
+
+  /// Last time the pipeline was triggered
+  final DateTime lastTrigger;
+
+  /// Is the pipeline enabled
+  final bool enabled;
+
+  // TODO Is the data type of the parameters correct?
+  // The parameters
+  final Object parameters;
+
+  // TODO How is the reaction linked, of the triggering service?
+
+  const Pipeline(
+      {Key? key,
+      required this.id,
+      required this.name,
+      required this.triggerCount,
+      required this.lastTrigger,
+      required this.enabled,
+      required this.parameters});
 }
