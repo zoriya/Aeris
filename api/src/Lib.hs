d4e0a501fb86b56d4173630a614e473661538830
Bluub <louis.auzuret@gmail.com>
2022-01-25 15:32:04 +0100

    feat: about.sjon with empty services and localhost as client ip

--- api/src/Lib.hs
+++ api/src/Lib.hs
@@ -7,10 +7,13 @@ module Lib
     ) where
 
 import Data.Aeson
-import Data.Aeson.TH
+import Data.Aeson.TH ( deriveJSON )
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import Data.Time.Clock.POSIX (getPOSIXTime, POSIXTime)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Aeson.Parser
 
 data User = User
   { userId        :: Int
@@ -18,9 +21,41 @@ data User = User
   , userLastName  :: String
   } deriving (Eq, Show)
 
+data ClientAbout = ClientAbout
+  { host :: String
+  } deriving (Eq, Show)
+
+data ActionAbout = ActionAbout
+  { actionName :: String
+  , description :: String
+  } deriving (Eq, Show)
+
+data ServicesAbout = ServicesAbout
+  { name :: String 
+  , actions :: [ActionAbout]
+  , reactions :: [ActionAbout]
+  } deriving (Eq, Show)
+
+data ServerAbout = ServerAbout
+  { current_time :: POSIXTime 
+  , services :: [ServicesAbout]
+  } deriving (Eq, Show)
+
+data About = About
+  { client :: ClientAbout,
+    serverAbout :: ServerAbout
+  } deriving (Eq, Show)
+
 $(deriveJSON defaultOptions ''User)
+$(deriveJSON defaultOptions ''ClientAbout)
+$(deriveJSON defaultOptions ''ActionAbout)
+$(deriveJSON defaultOptions ''ServicesAbout)
+$(deriveJSON defaultOptions ''ServerAbout)
+$(deriveJSON defaultOptions ''About)
+
 
 type API = "users" :> Get '[JSON] [User]
+        :<|> "about.json" :> Get '[JSON] About
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -33,8 +68,17 @@ api = Proxy
 
 server :: Server API
 server = return users
+    :<|> about
+    where
+        about :: Handler About
+        about = do
+            now <- liftIO getPOSIXTime
+            return $ about' now
 
 users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+
+about' :: POSIXTime -> About
+about' t = About (ClientAbout "localhost") (ServerAbout t [])
