968afcd269617b324db7bf306aa3d32d043d96f5
Arthi-chaud <arthur.jamet@gmail.com>
2022-01-26 14:17:25 +0100

    Card page: close button

--- mobile/lib/src/widgets/aeris_card_page.dart
+++ mobile/lib/src/widgets/aeris_card_page.dart
@@ -14,8 +14,34 @@ class AerisCardPage extends StatelessWidget {
           topLeft: Radius.circular(radius), topRight: Radius.circular(radius)),
       child: Container(
           color: Colors.white,
-          padding: const EdgeInsets.only(top: 40, bottom: 0),
-          child: body),
+          padding: const EdgeInsets.only(top: 30, bottom: 0),
+          child: Column(
+            children: [
+              Container(
+                child: const AerisPageCloseButton(),
+                alignment: Alignment.centerRight,
+              )
+            ],
+          )),
+    );
+  }
+}
+
+// Close button for aeris page
+class AerisPageCloseButton extends StatelessWidget {
+  const AerisPageCloseButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => Navigator.pop(context),
+      child:
+          Icon(Icons.close, color: Theme.of(context).colorScheme.onSecondary),
+      style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(5),
+          primary: Theme.of(context).colorScheme.secondary),
     );
   }
 }
