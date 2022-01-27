aa82f48026e2a4e35476034dd82d40e6cea2c37d
Bluub <louis.auzuret@gmail.com>
2022-01-27 16:00:00 +0100

    feat: basic connection to postgresql

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import qualified Hasql.Connection as Connection
 
 main :: IO ()
-main = startApp
+main = do
+    Right conn <- Connection.acquire connectionSettings
+    startApp
+    where
+        connectionSettings = Connection.settings "db" 5432 "postgres" "" "postgres"
