85b90c1f3bc63ee7781dde6ebd01f535ed17cf9c
Arthi-chaud <arthur.jamet@gmail.com>
2022-01-31 13:36:37 +0100

    Mobile client: setup page

--- mobile/lib/src/views/setup_action_page.dart
+++ mobile/lib/src/views/setup_action_page.dart
@@ -25,7 +25,40 @@ class _SetupActionPageState extends State<SetupActionPage> {
     final SetupActionPageArguments arguments =
         ModalRoute.of(context)!.settings.arguments as SetupActionPageArguments;
 
+    Map<String, <Map<String, String?>>> availableActions;
+
+    for (int i = 0; i <= 10; i++) {"action $i": {'key1': 'value1', 'key2': null}}
+    ;
+
     aeris.Action action = arguments.action;
+
+    final Widget serviceDropdown = DropdownButton<Service>(
+      value: action.service,
+      elevation: 8,
+      underline: Container(
+        height: 1,
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+      onChanged: (service) {
+        print("Selected ${service!.name}");
+        setState(() {
+          // TODO Setstate + call api to get available actions
+        });
+      },
+      items: Service.all().map<DropdownMenuItem<Service>>((Service service) {
+        return DropdownMenuItem<Service>(
+          value: service,
+          child: Row(children: [
+            service.getLogo(logoSize: 40),
+            const SizedBox(
+              width: 10,
+              height: 100,
+            ),
+            Text(service.name, style: TextStyle(fontSize: 20))
+          ]),
+        );
+      }).toList(),
+    );
 
     return AerisCardPage(
         body: Column(
@@ -35,37 +68,13 @@ class _SetupActionPageState extends State<SetupActionPage> {
               fontSize: 25,
             )),
         const SizedBox(height: 60),
+        Align(alignment: Alignment.centerRight, child: serviceDropdown),
         Align(
-          alignment: Alignment.centerRight,
-          child: DropdownButton<Service>(
-            value: action.service,
-            elevation: 8,
-            underline: Container(
-              height: 1,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            onChanged: (service) {
-              print("Selected ${service!.name}");
-              setState(() {
-                // TODO Setstate + call api to get available actions
-              });
-            },
-            items:
-                Service.all().map<DropdownMenuItem<Service>>((Service service) {
-              return DropdownMenuItem<Service>(
-                value: service,
-                child: Row(children: [
-                  service.getLogo(logoSize: 40),
-                  const SizedBox(
-                    width: 10,
-                    height: 100,
-                  ),
-                  Text(service.name, style: TextStyle(fontSize: 20))
-                ]),
-              );
-            }).toList(),
-          ),
-        )
+            alignment: Alignment.centerLeft,
+            child: Text("${availableActions.length} available actions"))
+        const SizedBox(height: 10),
+        for (var action in availableActions)
+          ExpansionPanel( headerBuilder: (context, _) => Text(""),body: Container())
       ],
     ));
   }
