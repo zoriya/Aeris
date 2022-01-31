95e01988d9fbd48d2a2bbdc816a4489efe54953c
Your Name <clement.lebihan773@gmail.com>
2022-01-31 11:55:16 +0100

    discovering steppers

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import pB from "./components/PipelineBox"
 import PipelineBox from './components/PipelineBox';
+import PipelineCreation from './components/PipelineCreation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PipelineCreation />
         <PipelineBox title='Like me' lastExecutionTime={"10 min"} />
       </header>
     </div>
