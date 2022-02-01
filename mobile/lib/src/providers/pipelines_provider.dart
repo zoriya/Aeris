fb69069a07c010f3adbbee6c07a9d53d03b1d373
Arthi-chaud <arthur.jamet@gmail.com>
2022-02-01 14:11:01 +0100

    Mobile Client: Providers: move static data out of home_page

--- mobile/lib/src/providers/pipelines_provider.dart
+++ mobile/lib/src/providers/pipelines_provider.dart
@@ -1,12 +1,65 @@
 import 'package:mobile/src/models/pipeline.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:io';
+import 'package:mobile/src/models/reaction.dart';
+import 'package:mobile/src/models/service.dart';
+import 'package:mobile/src/models/trigger.dart';
 
 /// Provider class for Pipelines
 class PipelineProvider extends ChangeNotifier {
-
   /// List of Pipelines stored in Provider
-  List<Pipeline> pipelines = [];
+  late List<Pipeline> pipelines;
+
+  PipelineProvider() {
+    var trigger1 = Trigger(
+        service: const Service.spotify(),
+        action: "Play song",
+        last: DateTime.now());
+    var trigger3 = Trigger(
+        service: const Service.discord(),
+        action: "Send a message",
+        last: DateTime.now());
+    var trigger2 = Trigger(
+        service: const Service.spotify(),
+        action: "Play song",
+        last: DateTime.parse("2022-01-01"));
+    var reaction = Reaction(
+        service: Service.twitter(), parameters: {}, name: "Post a tweet");
+    var pipeline1 = Pipeline(
+        id: 10,
+        name: "My Action",
+        triggerCount: 10,
+        enabled: true,
+        parameters: {},
+        trigger: trigger1,
+        reactions: [reaction]);
+    var pipeline2 = Pipeline(
+        id: 10,
+        name: "My very long action Action",
+        triggerCount: 10,
+        enabled: true,
+        trigger: trigger2,
+        parameters: {},
+        reactions: [reaction, reaction]);
+    var pipeline3 = Pipeline(
+        id: 10,
+        name: "Disabled",
+        triggerCount: 10,
+        enabled: false,
+        trigger: trigger3,
+        parameters: {},
+        reactions: [reaction]);
+    pipelines = [
+      pipeline3,
+      pipeline2,
+      pipeline1,
+      pipeline3,
+      pipeline2,
+      pipeline1,
+      pipeline3,
+      pipeline2,
+      pipeline1
+    ];
+  }
 
   /// Adds a pipeline in the Provider
   addPipelineInProvider(Pipeline newPipeline) {
@@ -49,5 +102,4 @@ class PipelineProvider extends ChangeNotifier {
     pipelines.clear();
     notifyListeners();
   }
-
 }
--- mobile/lib/src/views/home_page.dart
+++ mobile/lib/src/views/home_page.dart
@@ -1,11 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile/src/models/pipeline.dart';
-import 'package:mobile/src/models/reaction.dart';
-import 'package:mobile/src/models/service.dart';
-import 'package:mobile/src/models/trigger.dart';
 import 'package:mobile/src/providers/pipelines_provider.dart';
 import 'package:mobile/src/widgets/aeris_page.dart';
 import 'package:mobile/src/widgets/clickable_card.dart';
@@ -21,63 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    var trigger1 = Trigger(
-        service: const Service.spotify(),
-        action: "Play song",
-        last: DateTime.now());
-    var trigger3 = Trigger(
-        service: const Service.discord(),
-        action: "Send a message",
-        last: DateTime.now());
-    var trigger2 = Trigger(
-        service: const Service.spotify(),
-        action: "Play song",
-        last: DateTime.parse("2022-01-01"));
-    var reaction = Reaction(
-        service: Service.twitter(), parameters: {}, name: "Post a tweet");
-    var pipeline1 = Pipeline(
-        id: 10,
-        name: "My Action",
-        triggerCount: 10,
-        enabled: true,
-        parameters: {},
-        trigger: trigger1,
-        reactions: [reaction]);
-    var pipeline2 = Pipeline(
-        id: 10,
-        name: "My very long action Action",
-        triggerCount: 10,
-        enabled: true,
-        trigger: trigger2,
-        parameters: {},
-        reactions: [reaction, reaction]);
-    var pipeline3 = Pipeline(
-        id: 10,
-        name: "Disabled",
-        triggerCount: 10,
-        enabled: false,
-        trigger: trigger3,
-        parameters: {},
-        reactions: [reaction]);
-    List<Pipeline> pipelines = [
-      pipeline3,
-      pipeline2,
-      pipeline1,
-      pipeline3,
-      pipeline2,
-      pipeline1,
-      pipeline3,
-      pipeline2,
-      pipeline1
-    ];
-
-    PipelineProvider pipelineProvider =
-        Provider.of<PipelineProvider>(context, listen: false);
-    pipelineProvider.setPipelineProvider(pipelines);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
