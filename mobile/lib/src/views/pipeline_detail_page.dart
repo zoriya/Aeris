fa12b4f982333a453886604177f1017d13a76b95
Arthi-chaud <arthur.jamet@gmail.com>
2022-02-01 14:51:26 +0100

    Mobile Client: Detail page: notify listener on enable toggle

--- mobile/lib/src/views/pipeline_detail_page.dart
+++ mobile/lib/src/views/pipeline_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:mobile/src/providers/pipelines_provider.dart';
 import 'package:mobile/src/views/setup_action_page.dart';
 import 'package:mobile/src/widgets/aeris_popup_menu_item.dart';
 import 'package:mobile/src/widgets/aeris_popup_menu.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_switch/flutter_switch.dart';
 import 'package:mobile/src/models/reaction.dart';
 import 'package:mobile/src/models/pipeline.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// Class to get the pipeline's name in route's arguments
 class PipelineDetailPageArguments {
@@ -58,134 +60,138 @@ class PipelineDetailPage extends StatefulWidget {
 
 class _PipelineDetailPageState extends State<PipelineDetailPage> {
   @override
-  Widget build(BuildContext context) {
-    final PipelineDetailPageArguments arguments = ModalRoute.of(context)!
-        .settings
-        .arguments as PipelineDetailPageArguments;
+  Widget build(BuildContext context) =>
+      Consumer<PipelineProvider>(builder: (context, provider, _) {
+        final PipelineDetailPageArguments arguments = ModalRoute.of(context)!
+            .settings
+            .arguments as PipelineDetailPageArguments;
 
-    Pipeline pipeline = arguments.pipeline;
-    final cardHeader = Row(
-      children: [
-        Expanded(
-          flex: 7,
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(pipeline.name,
-                    style: const TextStyle(
-                      fontSize: 25,
-                    )),
-              ),
-              const SizedBox(height: 10),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(pipeline.trigger.lastToString(),
-                    style: const TextStyle(
-                      fontSize: 17,
-                    )),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.center,
-                  child: FlutterSwitch(
-                    activeColor: Colors.green,
-                    width: 60,
-                    value: pipeline.enabled,
-                    onToggle: (value) {
-                      setState(() {
-                        pipeline.enabled = !pipeline.enabled;
-                      });
-                    },
+        Pipeline pipeline = arguments.pipeline;
+        final cardHeader = Row(
+          children: [
+            Expanded(
+              flex: 7,
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(pipeline.name,
+                        style: const TextStyle(
+                          fontSize: 25,
+                        )),
                   ),
-                ),
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(pipeline.enabled ? "Enabled" : "Disabed",
-                      style: const TextStyle(fontSize: 13)),
-                ),
-              ],
-            ))
-      ],
-    );
-
-    final Widget addReactionbutton = ClickableCard(
-        color: Theme.of(context).colorScheme.secondaryContainer,
-        elevation: 5,
-        body: Container(
-            child: Text(
-              "Add a reaction",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(pipeline.trigger.lastToString(),
+                        style: const TextStyle(
+                          fontSize: 17,
+                        )),
+                  ),
+                ],
+              ),
             ),
-            width: double.infinity,
-            padding: const EdgeInsets.only(top: 15, bottom: 15)),
-        onTap: () {
-          print("add reaction pipeline"); // TODO add reaction
-        });
+            Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: FlutterSwitch(
+                        activeColor: Colors.green,
+                        width: 60,
+                        value: pipeline.enabled,
+                        onToggle: (value) {
+                          setState(() {
+                            pipeline.enabled = !pipeline.enabled;
+                            provider.notifyListeners();
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(pipeline.enabled ? "Enabled" : "Disabed",
+                          style: const TextStyle(fontSize: 13)),
+                    ),
+                  ],
+                ))
+          ],
+        );
 
-    final Widget deleteButton = ClickableCard(
-      color: Theme.of(context).colorScheme.error,
-      elevation: 5,
-      body: Container(
-          child: Text(
-            "Delete a Pipeline",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Theme.of(context).colorScheme.onSecondary,
-                fontSize: 20,
-                fontWeight: FontWeight.w600),
-          ),
-          width: double.infinity,
-          padding: const EdgeInsets.only(top: 15, bottom: 15)),
-      onTap: () => showDialog<String>(
-          context: context,
-          builder: (BuildContext context) => WarningDialog(
-              message:
-                  "You are about to delete a pipeline. This action can not be undone. Are you sure ?",
-              onAccept: () => print("Delete pipeline") /*TODO remove pipeline*/,
-              warnedAction: "Delete")),
-    );
+        final Widget addReactionbutton = ClickableCard(
+            color: Theme.of(context).colorScheme.secondaryContainer,
+            elevation: 5,
+            body: Container(
+                child: Text(
+                  "Add a reaction",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                width: double.infinity,
+                padding: const EdgeInsets.only(top: 15, bottom: 15)),
+            onTap: () {
+              print("add reaction pipeline"); // TODO add reaction
+            });
 
-    return AerisCardPage(
-        body: Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 40),
-          child: cardHeader,
-        ),
-        const Text("Action", style: TextStyle(fontWeight: FontWeight.w500)),
-        ActionCard(
-            leading: pipeline.trigger.service.getLogo(logoSize: 50),
-            title: pipeline.trigger.name,
-            trailing: widget.actionPopupMenu(pipeline.trigger, context)),
-        const SizedBox(height: 25),
-        const Text("Reactions", style: TextStyle(fontWeight: FontWeight.w500)),
-        for (var reaction in pipeline.reactions)
-          ActionCard(
-              leading: reaction.service.getLogo(logoSize: 50),
-              title: reaction.name,
-              trailing: widget.actionPopupMenu(reaction, context)),
-        addReactionbutton,
-        const Padding(
-            padding: EdgeInsets.only(top: 30, bottom: 5),
-            child: Text("Danger Zone",
-                style: TextStyle(fontWeight: FontWeight.w500))),
-        deleteButton,
-        const SizedBox(height: 25),
-      ]),
-    ));
-  }
+        final Widget deleteButton = ClickableCard(
+          color: Theme.of(context).colorScheme.error,
+          elevation: 5,
+          body: Container(
+              child: Text(
+                "Delete a Pipeline",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              width: double.infinity,
+              padding: const EdgeInsets.only(top: 15, bottom: 15)),
+          onTap: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => WarningDialog(
+                  message:
+                      "You are about to delete a pipeline. This action can not be undone. Are you sure ?",
+                  onAccept: () =>
+                      print("Delete pipeline") /*TODO remove pipeline*/,
+                  warnedAction: "Delete")),
+        );
+
+        return AerisCardPage(
+            body: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: ListView(children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: cardHeader,
+            ),
+            const Text("Action", style: TextStyle(fontWeight: FontWeight.w500)),
+            ActionCard(
+                leading: pipeline.trigger.service.getLogo(logoSize: 50),
+                title: pipeline.trigger.name,
+                trailing: widget.actionPopupMenu(pipeline.trigger, context)),
+            const SizedBox(height: 25),
+            const Text("Reactions",
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            for (var reaction in pipeline.reactions)
+              ActionCard(
+                  leading: reaction.service.getLogo(logoSize: 50),
+                  title: reaction.name,
+                  trailing: widget.actionPopupMenu(reaction, context)),
+            addReactionbutton,
+            const Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 5),
+                child: Text("Danger Zone",
+                    style: TextStyle(fontWeight: FontWeight.w500))),
+            deleteButton,
+            const SizedBox(height: 25),
+          ]),
+        ));
+      });
 }
