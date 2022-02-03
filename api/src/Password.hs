5e4519a7a0d386818ee7feefb23bcb865c6eabfb
Bluub <louis.auzuret@gmail.com>
2022-02-03 12:07:22 +0100

    feat: adding cryptonite package

--- api/src/Password.hs
+++ api/src/Password.hs
@@ -7,9 +7,15 @@ import Rel8 ( DBEq, DBType )
 import Data.Aeson ( FromJSON, ToJSON )
 import Data.Text ( Text )
 
+import Crypto.KDF.BCrypt
 
 newtype HashedPassword = HashedPassword { getHashedPasswd :: Text }
     deriving newtype (Eq, Show, Read, DBEq, DBType)
 
 newtype Password = Password { getPassword :: Text }
     deriving newtype (Eq, Show, Read, FromJSON, ToJSON, DBEq, DBType)
+
+-- TODO Check if the password meets minimum security requirements
+toPassword :: Text -> Password
+toPassword = Password
+
