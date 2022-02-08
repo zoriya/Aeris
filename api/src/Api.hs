b75010083201fcaa6902dcd16c5db428b285ff1a
Bluub <louis.auzuret@gmail.com>
2022-02-08 11:53:14 +0100

    fix: useless imports

--- api/src/Api.hs
+++ api/src/Api.hs
@@ -5,8 +5,6 @@
 
 module Api where
 
-import Data.Aeson
-import Data.Aeson.TH ( deriveJSON )
 import Servant.Auth.Server (CookieSettings, JWTSettings, JWT)
 import Servant.API.Generic        ((:-), ToServantApi)
 import Servant ( JSON, NamedRoutes, RemoteHost, type (:>), Get, HasServer (ServerT), Handler )
@@ -30,11 +28,11 @@ type NamedAPI = NamedRoutes API
 
 
 server :: CookieSettings -> JWTSettings -> ServerT NamedAPI AppM
-server cs jwts = API {
-    Api.users = Api.User.users
-    , Api.about = Api.About.about
-    , Api.auth = Api.Auth.authHandler cs jwts
-}
+server cs jwts = API
+  { Api.users = Api.User.users
+  , Api.about = Api.About.about
+  , Api.auth = Api.Auth.authHandler cs jwts
+  }
 
 nt :: State -> AppM a -> Handler a
 nt s x = runReaderT x s
