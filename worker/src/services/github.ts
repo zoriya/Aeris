2ba33c6d200d1c7ad8810532dc39b313326e8baf
Arthi-chaud <arthur.jamet@gmail.com>
2022-02-14 14:22:31 +0100

    Worker: Github: Comment on Pull request Reaction

--- worker/src/services/github.ts
+++ worker/src/services/github.ts
@@ -1,14 +1,20 @@
 import { Octokit } from "@octokit/rest";
 
 export class GitHub {
+
+	private static getGitHubInstance() {
+		return new Octokit();
+	}
+
 	static commentPR(params: any) {
-		var octokit = new Octokit();
 		var expectedParameters = ['owner', 'repo', 'pull_number', 'body']
 		expectedParameters.forEach((param, _, __) => {
 			if (!(param in params))
 				throw new MissingParameterException(param);
 		});
-		octokit.pulls.createReviewComment(params['owner'], params['repo'], params['pull_number'], params['body'], bod);
-
+		this.getGitHubInstance().pulls.createReviewComment({
+			owner: params['owner'], repo: params['repo'], 
+			pull_number: params['pull_number'], body: params['body']
+		});
 	}
 }
