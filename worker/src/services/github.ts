b27330408fabf22d7386c824e1eaebc0e3dbbd84
Arthi-chaud <arthur.jamet@gmail.com>
2022-02-14 14:58:58 +0100

    Worker: Github: Comment on IssueReaction

--- worker/src/services/github.ts
+++ worker/src/services/github.ts
@@ -17,4 +17,16 @@ export class GitHub {
 			pull_number: params['pull_number'], body: params['body']
 		});
 	}
+
+	static commentIssue(params: any) {
+		var expectedParameters = ['owner', 'repo', 'issue_number', 'body']
+		expectedParameters.forEach((param, _, __) => {
+			if (!(param in params))
+				throw new MissingParameterException(param);
+		});
+		this.getGitHubInstance().issues.createComment({
+			owner: params['owner'], repo: params['repo'], 
+			issue_number: params['issue_number'], body: params['body']
+		});
+	}
 }
