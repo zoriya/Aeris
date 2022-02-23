28d2f5e78dc63f62879370d0462ab5c2d19e3232
Arthi-chaud <arthur.jamet@gmail.com>
2022-02-23 09:57:56 +0100

    Mobile Client: Action: 'serialise' action type

--- mobile/lib/src/models/action.dart
+++ mobile/lib/src/models/action.dart
@@ -21,9 +21,13 @@ abstract class Action {
 
   static Tuple2<Service, String> parseServiceAndName(String rType) {
     var snake = ReCase(rType).snakeCase.split('_');
-    return Tuple2(
-      Service.factory(snake.first),
-      ReCase(snake.getRange(1, snake.length - 1).join('_')).titleCase
-    );
+    return Tuple2(Service.factory(snake.first),
+        ReCase(snake.getRange(1, snake.length - 1).join('_')).titleCase);
+  }
+
+  static String getType(Service service, String aName) {
+    String serviceName = ReCase(service.name).pascalCase;
+    String actionName = ReCase(aName).paramCase;
+    return "$serviceName$actionName";
   }
 }
