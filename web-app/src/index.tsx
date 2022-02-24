d45c16e56f1718f0ecdd221c5833222a6decb71b
0Nom4D <arthur.adam@epitech.eu>
2022-02-24 18:09:40 +0100

    Aeris : creation_pipeline_full - Adding routes inside index.tsx

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -5,6 +5,9 @@ import "./App.css";
 import App from "./App";
 import GithubAuth from "./components/Authorizations/GithubAuth";
 import SpotifyAuth from "./components/Authorizations/SpotifyAuth";
+import YouTubeAuth from "./components/Authorizations/YoutubeAuth";
+import TwitterAuth from "./components/Authorizations/TwitterAuth";
+import DiscordAuth from "./components/Authorizations/DiscordAuth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthComponent from "./pages/Login/LoginPage";
 import PipelinePage from "./pages/HomePage";
@@ -30,6 +33,9 @@ function AerisRouter() {
 							<Route path="/pipelines" element={<PipelinePage />} />
 							<Route path='/authorization/github' element={<GithubAuth />} />
 							<Route path='/authorization/spotify' element={<SpotifyAuth />} />
+							<Route path='/authorization/youtube' element={<YouTubeAuth />} />
+							<Route path='/authorization/twitter' element={<TwitterAuth />} />
+							<Route path='/authorization/discord' element={<DiscordAuth />} />
 						</Routes>
 					</BrowserRouter>
 				</header>
