c8a3d99cae74dcdf78ac31b0f7e2870d17201240
GitBluub <louis.auzuret@gmail.com>
2022-02-25 17:19:38 +0100

    WIP github oidc

--- api/src/Api/OIDC.hs
+++ api/src/Api/OIDC.hs
@@ -8,19 +8,19 @@ import App (AppM)
 import Control.Monad.IO.Class (liftIO)
 import Core.User (ExternalToken (ExternalToken, service), Service (Github), UserId (UserId), User (User))
 import Data.Text (pack)
-import OIDC
+import Core.OIDC ( getOauthTokens )
 import Repository.User (updateTokens)
-import Servant (Capture, Get, GetNoContent, JSON, NoContent (NoContent), QueryParam, ServerT, err400, throwError, type (:<|>) ((:<|>)), type (:>), err401)
+import Servant (Capture, Get, GetNoContent, JSON, NoContent (NoContent), QueryParam, ServerT, err400, throwError, type (:<|>) ((:<|>)), type (:>), err401, err403)
 import Servant.API.Generic (type (:-))
 import Servant.Server.Generic (AsServerT)
 import Utils (UserAuth, AuthRes)
 import Servant.Auth.Server (AuthResult(Authenticated))
 
 oauthHandler :: AuthRes -> Service -> Maybe String -> AppM NoContent
-oauthHandler (Authenticated (User uid name slug)) service (Just code) = do
+oauthHandler (Authenticated (User uid _ _)) service (Just code) = do
     tokens <- liftIO $ getOauthTokens service code
     case tokens of
-        Nothing -> throwError err400
+        Nothing -> throwError err403 
         Just t -> do
             updateTokens uid t
             return NoContent
