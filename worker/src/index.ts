72c3c473b2bb76a5faeac56d5286c318f3e7c568
Zoe Roux <zoe.roux@sdg.moe>
2022-03-01 14:22:33 +0100

    Fixing workers api fetch

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,4 @@
-import { fromEvent, merge, Observable } from "rxjs";
+import { fromEvent, mergeAll, mergeWith, Observable } from "rxjs";
 import { fromFetch } from 'rxjs/fetch';
 import { Manager } from "./actions";
 import "./services";
@@ -14,8 +14,8 @@ global.AbortController = AbortController;
 
 const app = express()
 const pipelineEvent = new EventEmitter();
-
 app.put("/workflow/:id", req => {
+
 	fetch(`${process.env["WORKER_API_URL"]}/workflow/${req.params.id}?WORKER_API_KEY=${process.env["WORKER_API_KEY"]}`)
 		.then(res => {
 			pipelineEvent.emit("event", res.json());
@@ -38,15 +38,12 @@ app.delete("/workflow/:id", req => {
 
 app.listen(5000);
 
-console.log(`${process.env["WORKER_API_URL"]}/workflows?WORKER_API_KEY=${process.env["WORKER_API_KEY"]}`)
 
-fetch(`${process.env["WORKER_API_URL"]}/workflows?WORKER_API_KEY=${process.env["WORKER_API_KEY"]}`)
-	.then(async res => console.log("toto", await res.json()))
-
-const pipelines = merge(
-	fromFetch<Pipeline>(`${process.env["WORKER_API_URL"]}/workflows?WORKER_API_KEY=${process.env["WORKER_API_KEY"]}`, {selector: x => x.json()}),
-	fromEvent(pipelineEvent, "event"),
-) as Observable<Pipeline>;
+const pipelines = fromFetch<Pipeline[]>(`${process.env["WORKER_API_URL"]}/workflows?WORKER_API_KEY=${process.env["WORKER_API_KEY"]}`, {selector: x => x.json()})
+		.pipe(
+			mergeAll(),
+			mergeWith(fromEvent(pipelineEvent, "event")),
+		) as Observable<Pipeline>;
 
 const manager: Manager = new Manager(pipelines);
 await manager.run()
