03dfe47b6a70705f4e90fb12d48bc46db9884702
GitBluub <louis.auzuret@gmail.com>
2022-03-02 10:56:59 +0100

    feat: service route takes redirect uri as query param

--- api/src/Core/OIDC.hs
+++ api/src/Core/OIDC.hs
@@ -40,10 +40,9 @@ getGithubConfig =
         <*> envAsString "GITHUB_SECRET" ""
         <*> pure "https://github.com/login/oauth/access_token"
 
-getGithubTokens :: String -> IO (Maybe ExternalToken)
-getGithubTokens code = do
+getGithubTokens :: String -> String -> IO (Maybe ExternalToken)
+getGithubTokens code _ = do
     gh <- getGithubConfig
-    print gh
     let endpoint = tokenEndpoint code gh
     request' <- parseRequest endpoint
     let request =
@@ -56,7 +55,6 @@ getGithubTokens code = do
                 ]
             request'
     response <- httpJSONEither request
-    print response
     return $ case (getResponseBody response :: Either JSONException Object) of
         Left _ -> Nothing
         Right obj -> do
@@ -72,8 +70,8 @@ getDiscordConfig =
         <*> envAsString "DISCORD_SECRET" ""
         <*> pure "https://discord.com/api/oauth2/token"
 
-getDiscordTokens :: String -> IO (Maybe ExternalToken)
-getDiscordTokens code = do
+getDiscordTokens :: String -> String -> IO (Maybe ExternalToken)
+getDiscordTokens code redirect = do
     cfg <- getDiscordConfig
     let endpoint = tokenEndpoint code cfg
     request' <- parseRequest endpoint
@@ -85,7 +83,7 @@ getDiscordTokens code = do
                 , ("client_secret", B8.pack . oauthClientSecret $ cfg)
                 , ("code", B8.pack code)
                 , ("grant_type", "authorization_code")
-                , ("redirect_uri", "http://localhost:3000/authorization/discord")
+                , ("redirect_uri", B8.pack redirect)
                 ]
             request'
     response <- httpJSONEither request
@@ -104,8 +102,8 @@ getGoogleConfig =
         <*> envAsString "GOOGLE_SECRET" ""
         <*> pure "https://oauth2.googleapis.com/token"
 
-getGoogleTokens :: String -> IO (Maybe ExternalToken)
-getGoogleTokens code = do
+getGoogleTokens :: String -> String -> IO (Maybe ExternalToken)
+getGoogleTokens code redirect = do
     cfg <- getGoogleConfig
     let endpoint = tokenEndpoint code cfg
     request' <- parseRequest endpoint
@@ -117,7 +115,7 @@ getGoogleTokens code = do
                 , ("client_secret", B8.pack . oauthClientSecret $ cfg)
                 , ("code", B8.pack code)
                 , ("grant_type", "authorization_code")
-                , ("redirect_uri", "http://localhost:3000/authorization/google")
+                , ("redirect_uri", B8.pack redirect)
                 ]
             request'
     response <- httpJSONEither request
@@ -136,8 +134,8 @@ getSpotifyConfig =
         <*> envAsString "SPOTIFY_SECRET" ""
         <*> pure "https://accounts.spotify.com/api/token"
 
-getSpotifyTokens :: String -> IO (Maybe ExternalToken)
-getSpotifyTokens code = do
+getSpotifyTokens :: String -> String -> IO (Maybe ExternalToken)
+getSpotifyTokens code redirect = do
     cfg <- getSpotifyConfig
 
     let basicAuth = encodeBase64 $ B8.pack $ oauthClientId cfg ++ ":" ++ oauthClientSecret cfg
@@ -145,12 +143,12 @@ getSpotifyTokens code = do
     request' <- parseRequest endpoint
     let request =
             setRequestMethod "POST" $
-            addRequestHeader "Authorization" (B8.pack $ "Basic " ++ (unpack $ basicAuth)) $
+            addRequestHeader "Authorization" (B8.pack $ "Basic " ++ unpack basicAuth) $
             addRequestHeader "Accept" "application/json" $
             setRequestBodyURLEncoded
                 [ ("code", B8.pack code)
                 , ("grant_type", "authorization_code")
-                , ("redirect_uri", "http://localhost:3000/authorization/spotify")
+                , ("redirect_uri", B8.pack redirect)
                 ]
             request'
     response <- httpJSONEither request
@@ -169,8 +167,8 @@ getTwitterConfig =
         <*> envAsString "TWITTER_SECRET" ""
         <*> pure "https://api.twitter.com/2/oauth2/token"
 
-getTwitterTokens :: String -> IO (Maybe ExternalToken)
-getTwitterTokens code = do
+getTwitterTokens :: String -> String -> IO (Maybe ExternalToken)
+getTwitterTokens code redirect = do
     cfg <- getTwitterConfig
     let basicAuth = encodeBase64 $ B8.pack $ "Basic " ++ oauthClientId cfg ++ ":" ++ oauthClientSecret cfg
     let endpoint = tokenEndpoint code cfg
@@ -182,7 +180,7 @@ getTwitterTokens code = do
             setRequestBodyURLEncoded
                 [ ("code", B8.pack code)
                 , ("grant_type", "authorization_code")
-                , ("redirect_uri", "http://localhost:3000/authorization/twitter")
+                , ("redirect_uri", B8.pack redirect)
                 , ("code_verifier", "challenge")
                 ]
             request'
@@ -202,8 +200,8 @@ getAnilistConfig =
         <*> envAsString "ANILIST_SECRET" ""
         <*> pure "https://anilist.co/api/v2/oauth/token"
 
-getAnilistTokens :: String -> IO (Maybe ExternalToken)
-getAnilistTokens code = do
+getAnilistTokens :: String -> String -> IO (Maybe ExternalToken)
+getAnilistTokens code redirect = do
     cfg <- getAnilistConfig
     let endpoint = tokenEndpoint code cfg
     request' <- parseRequest endpoint
@@ -215,7 +213,7 @@ getAnilistTokens code = do
                 , ("client_secret", B8.pack . oauthClientSecret $ cfg)
                 , ("code", B8.pack code)
                 , ("grant_type", "authorization_code")
-                , ("redirect_uri", "http://localhost:3000/authorization/anilist")
+                , ("redirect_uri", B8.pack redirect)
                 ]
             request'
     response <- httpJSONEither request
@@ -230,7 +228,7 @@ getAnilistTokens code = do
 
 
 -- General
-getOauthTokens :: Service -> String -> IO (Maybe ExternalToken)
+getOauthTokens :: Service -> String -> String -> IO (Maybe ExternalToken)
 getOauthTokens Github = getGithubTokens
 getOauthTokens Discord = getDiscordTokens
 getOauthTokens Spotify = getSpotifyTokens
