10bbda3e5c5a6178d85c9c5219b970655b6c85cc
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-03-03 21:54:37 +0100

    reworking linked service list

--- web-app/src/pages/ServiceSetup.tsx
+++ web-app/src/pages/ServiceSetup.tsx
@@ -19,84 +19,76 @@ export default function ServiceSetupModal({ services, setServices }: ServiceSetu
 	const unlinkedServices = services.filter((el) => !el.linked);
 	const linkedServices = services.filter((el) => el.linked);
 	return (
-		<div>
+		<div
+			style={{
+				display: "grid",
+				gridTemplateColumns: "30vw 5vw 30vw",
+				gridTemplateRows: "2fr 1fr 18fr",
+				gridTemplateAreas: `
+					'WindowTitle  WindowTitle WindowTitle'
+					'LinkedTitle  .           UnlinkedTitle'
+					'LinkedList   .           UnlinkedList'
+				`,
+				alignItems: "center",
+			}}>
 			<Box
 				sx={{
-					display: "flex",
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-					marginBottom: "25px",
+					gridArea: "WindowTitle",
 				}}>
 				<Typography variant="h4" noWrap align="left">
 					Services
 				</Typography>
 			</Box>
-			<Grid container spacing={4}>
-				<Grid item xs container direction="column" spacing={2}>
-					<Grid item>
-						<Typography variant="h6" noWrap align="left">
-							{t("linked")}
-						</Typography>
-						<Box
-							sx={{
-								flexDirection: "column",
-								alignItems: "center",
-								justifyContent: "space-between",
-								marginRight: "10px",
-							}}>
-							{linkedServices.map((elem, index) => (
-								<Grid item mb={4} key={index}>
-									<GenericButton
-										service={elem.logo}
-										title={elem.label}
-										trailingIcon={<Logout />}
-										onClickCallback={async () => {
-											if (await unLinkService(elem)) {
-												setServices(
-													services.map((svc) => {
-														if (svc.uid !== elem.uid) return svc;
-														return {
-															...svc,
-															linked: false,
-														};
-													})
-												);
-											}
-										}}
-									/>
-								</Grid>
-							))}
-						</Box>
+
+			<Typography sx={{ gridArea: "LinkedTitle" }} variant="h6" noWrap>
+				{t("linked")}
+			</Typography>
+
+			<Grid gridArea={"LinkedTitle"} item container direction="column" spacing={2}>
+				{linkedServices.map((elem, index) => (
+					<Grid item mb={4} key={index}>
+						<GenericButton
+							service={elem.logo}
+							title={elem.label}
+							trailingIcon={<Logout />}
+							onClickCallback={async () => {
+								if (await unLinkService(elem)) {
+									setServices(
+										services.map((svc) => {
+											if (svc.uid !== elem.uid) return svc;
+											return {
+												...svc,
+												linked: false,
+											};
+										})
+									);
+								}
+							}}
+						/>
 					</Grid>
-				</Grid>
-				<Grid item xs={12} sm container>
-					<Grid item xs container direction="column" spacing={2}>
-						<Grid item xs>
-							<Typography variant="h6" noWrap align="right">
-								{t("available")}
-							</Typography>
-							<Box
-								sx={{
-									flexDirection: "column",
-									alignItems: "center",
-									justifyContent: "space-between",
-									marginRight: "10px",
-								}}>
-								{unlinkedServices.map((elem, index) => (
-									<Grid item mb={4} key={index}>
-										<GenericButton
-											service={elem.logo}
-											title={elem.label}
-											trailingIcon={<Login />}
-											onClickCallback={() => (window.location.href = elem.urlAuth)}
-										/>
-									</Grid>
-								))}
-							</Box>
-						</Grid>
+				))}
+			</Grid>
+
+			<Typography sx={{ gridArea: "UnlinkedTitle" }} variant="h6" noWrap>
+				{t("available")}
+			</Typography>
+			<Grid
+				sx={{
+					gridArea: "UnlinkedList",
+				}}
+				container
+				direction="column"
+				spacing={2}>
+				{unlinkedServices.map((elem, index) => (
+					<Grid item key={index}>
+						<GenericButton
+							service={elem.logo}
+							title={elem.label}
+							trailingIcon={<Login />}
+							onClickCallback={() => (window.location.href = elem.urlAuth)}
+						/>
 					</Grid>
-				</Grid>
+				))}
 			</Grid>
 		</div>
 	);
