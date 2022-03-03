aff8fd745145729c1a696099c50b0c4c30983ffb
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-03-03 15:38:03 +0100

    adding modals for AREAs selections

--- web-app/src/pages/PipelineEdit/PipelineEditPage.tsx
+++ web-app/src/pages/PipelineEdit/PipelineEditPage.tsx
@@ -1,5 +1,6 @@
 import { AppAREAType, AppPipelineInfoType, AppPipelineType, AppServiceType } from "../../utils/types";
 import { useState } from "react";
+import PipelineModal from "../../components/Pipelines/PipelineModal";
 
 import PipelineEditPipeline from "./PipelineEditPipeline";
 import PipelineEditAREA from "./PipelineEditAREA";
@@ -47,21 +48,22 @@ export default function PipelineEditPage({
 				<PipelineEditPipeline
 					disableDeletion={disableDeletion}
 					pipelineData={editPipelineData}
-					handleEditPipelineTitle={(newTtitle) => setEditPipelineData({
-						...editPipelineData,
-						name: newTtitle
-					})}
+					handleEditPipelineTitle={(newTtitle) =>
+						setEditPipelineData({
+							...editPipelineData,
+							name: newTtitle,
+						})
+					}
 					handleEditPipelineMetaData={(name, enabled) => {
 						setEditPipelineData({
 							...editPipelineData,
 							name: name,
 							data: {
 								...editPipelineData.data,
-								enabled: enabled
-							}
-						})
-					}
-					}
+								enabled: enabled,
+							},
+						});
+					}}
 					handleEditAction={(action) => {
 						setEditActionData(action);
 						setMode(PipelineEditMode.EditAction);
@@ -87,77 +89,101 @@ export default function PipelineEditPage({
 			);
 		case PipelineEditMode.Action:
 			return (
-				<PipelineEditAREA
-					pipelineData={editPipelineData}
-					setEditMode={setMode}
-					isActions={true}
-					setAREA={(AREA: AppAREAType) => {
-						setEditPipelineData({
-							...editPipelineData,
-							action: AREA,
-						});
+				<PipelineModal
+					isOpen={true}
+					handleClose={() => {
 						setMode(PipelineEditMode.Pipeline);
-					}}
-					services={services}
-					AREAs={actions}
-				/>
+					}}>
+					<PipelineEditAREA
+						pipelineData={editPipelineData}
+						setEditMode={setMode}
+						isActions={true}
+						setAREA={(AREA: AppAREAType) => {
+							setEditPipelineData({
+								...editPipelineData,
+								action: AREA,
+							});
+							setMode(PipelineEditMode.Pipeline);
+						}}
+						services={services}
+						AREAs={actions}
+					/>
+				</PipelineModal>
 			);
 		case PipelineEditMode.Reactions:
 			return (
-				<PipelineEditAREA
-					pipelineData={editPipelineData}
-					setEditMode={setMode}
-					isActions={false}
-					setAREA={(AREA: AppAREAType) => {
-						let reactionsTmp = editPipelineData.reactions;
-						reactionsTmp[editReactionIndex] = AREA;
-						setEditPipelineData({
-							...editPipelineData,
-							reactions: reactionsTmp,
-						});
+				<PipelineModal
+					isOpen={true}
+					handleClose={() => {
 						setMode(PipelineEditMode.Pipeline);
-					}}
-					services={services}
-					AREAs={reactions}
-				/>
+					}}>
+					<PipelineEditAREA
+						pipelineData={editPipelineData}
+						setEditMode={setMode}
+						isActions={false}
+						setAREA={(AREA: AppAREAType) => {
+							let reactionsTmp = editPipelineData.reactions;
+							reactionsTmp[editReactionIndex] = AREA;
+							setEditPipelineData({
+								...editPipelineData,
+								reactions: reactionsTmp,
+							});
+							setMode(PipelineEditMode.Pipeline);
+						}}
+						services={services}
+						AREAs={reactions}
+					/>
+				</PipelineModal>
 			);
 		case PipelineEditMode.EditAction:
 			return (
-				<PipelineEditAREA
-					pipelineData={editPipelineData}
-					setEditMode={setMode}
-					isActions={true}
-					setAREA={(AREA: AppAREAType) => {
-						setEditPipelineData({
-							...editPipelineData,
-							action: AREA,
-						});
+				<PipelineModal
+					isOpen={true}
+					handleClose={() => {
 						setMode(PipelineEditMode.Pipeline);
-					}}
-					selectedAREA={editActionData}
-					services={services}
-					AREAs={actions}
-				/>
+					}}>
+					<PipelineEditAREA
+						pipelineData={editPipelineData}
+						setEditMode={setMode}
+						isActions={true}
+						setAREA={(AREA: AppAREAType) => {
+							setEditPipelineData({
+								...editPipelineData,
+								action: AREA,
+							});
+							setMode(PipelineEditMode.Pipeline);
+						}}
+						selectedAREA={editActionData}
+						services={services}
+						AREAs={actions}
+					/>
+				</PipelineModal>
 			);
 		case PipelineEditMode.EditReaction:
 			return (
-				<PipelineEditAREA
-					pipelineData={editPipelineData}
-					setEditMode={setMode}
-					isActions={false}
-					setAREA={(AREA: AppAREAType) => {
-						let reactionsTmp = editPipelineData.reactions;
-						reactionsTmp[editReactionIndex] = AREA;
-						setEditPipelineData({
-							...editPipelineData,
-							reactions: reactionsTmp,
-						});
+				<PipelineModal
+					isOpen={true}
+					handleClose={() => {
 						setMode(PipelineEditMode.Pipeline);
-					}}
-					selectedAREA={editReactionData}
-					services={services}
-					AREAs={reactions}
-				/>
+					}}>
+					<PipelineEditAREA
+						pipelineData={editPipelineData}
+						setEditMode={setMode}
+						isActions={false}
+						setAREA={(AREA: AppAREAType) => {
+							let reactionsTmp = editPipelineData.reactions;
+							reactionsTmp[editReactionIndex] = AREA;
+							setEditPipelineData({
+								...editPipelineData,
+								reactions: reactionsTmp,
+							});
+							setMode(PipelineEditMode.Pipeline);
+						}}
+						selectedAREA={editReactionData}
+						services={services}
+						AREAs={reactions}
+					/>
+				</PipelineModal>
 			);
 	}
 }
