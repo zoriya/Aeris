1996a75771dba183846370b5d95643f7d075eb51
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-03-03 23:36:22 +0100

    sections aren't shown if no pipelines are in them

--- web-app/src/components/Pipelines/PipelineSquaresLayout.tsx
+++ web-app/src/components/Pipelines/PipelineSquaresLayout.tsx
@@ -19,36 +19,44 @@ export const PipelineSquaresLayout = ({ data, style }: PipelineSquaresLayout) =>
 				height: "100%",
 				padding: "100px 20px 10px 20px",
 			}}>
-			<Stack direction={"row"} spacing={1} alignItems="center" style={{ margin: "20px" }}>
-				<Typography variant="h3" align="left">
-					My Pipelines
-				</Typography>
-				<Avatar sx={{ float: "left", bgcolor: theme.palette.secondary.main, width: 30, height: 30, fontSize: 20 }}>
-					{enabledPipelines.length}
-				</Avatar>
-			</Stack>
-			<Grid container direction="row" spacing={2} justifyContent="flex-start" alignItems="flex-start">
-				{enabledPipelines.map((el, index) => (
-					<Grid item key={index}>
-						<PipelineSquare {...el} />
+			{enabledPipelines.length > 0 && (
+				<>
+					<Stack direction={"row"} spacing={1} alignItems="center" style={{ margin: "20px" }}>
+						<Typography variant="h3" align="left">
+							My Pipelines
+						</Typography>
+						<Avatar sx={{ float: "left", bgcolor: theme.palette.secondary.main, width: 30, height: 30, fontSize: 20 }}>
+							{enabledPipelines.length}
+						</Avatar>
+					</Stack>
+					<Grid container direction="row" spacing={2} justifyContent="flex-start" alignItems="flex-start">
+						{enabledPipelines.map((el, index) => (
+							<Grid item key={index}>
+								<PipelineSquare {...el} />
+							</Grid>
+						))}
 					</Grid>
-				))}
-			</Grid>
-			<Stack direction={"row"} spacing={1} alignItems="center" style={{ margin: "25px 0 10px 20px" }}>
-				<Typography variant="h5" align="left">
-					Disabled pipelines
-				</Typography>
-				<Avatar sx={{ float: "left", bgcolor: theme.palette.secondary.main, width: 24, height: 24, fontSize: 15 }}>
-					{disabledPipelines.length}
-				</Avatar>
-			</Stack>
-			<Grid container direction="row" spacing={2} justifyContent="flex-start" alignItems="flex-start">
-				{disabledPipelines.map((el, index) => (
-					<Grid item key={index}>
-						<PipelineSquare {...el} />
+				</>
+			)}
+			{disabledPipelines.length > 0 && (
+				<>
+					<Stack direction={"row"} spacing={1} alignItems="center" style={{ margin: "25px 0 10px 20px" }}>
+						<Typography variant="h5" align="left">
+							Disabled pipelines
+						</Typography>
+						<Avatar sx={{ float: "left", bgcolor: theme.palette.secondary.main, width: 24, height: 24, fontSize: 15 }}>
+							{disabledPipelines.length}
+						</Avatar>
+					</Stack>
+					<Grid container direction="row" spacing={2} justifyContent="flex-start" alignItems="flex-start">
+						{disabledPipelines.map((el, index) => (
+							<Grid item key={index}>
+								<PipelineSquare {...el} />
+							</Grid>
+						))}
 					</Grid>
-				))}
-			</Grid>
+				</>
+			)}
 		</div>
 	);
 };
