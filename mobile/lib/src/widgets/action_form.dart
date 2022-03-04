226ba7a9613ea4e088d478743bf00453cbf9e893
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-04 03:10:44 +0100

    Mobile Client: Form: inherit work on cascade

--- mobile/lib/src/widgets/action_form.dart
+++ mobile/lib/src/widgets/action_form.dart
@@ -5,12 +5,16 @@ import 'package:aeris/src/models/action.dart' as aeris;
 import 'package:aeris/src/models/trigger.dart';
 import 'package:aeris/src/providers/action_catalogue_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:recase/recase.dart';
 import 'package:tuple/tuple.dart';
+
+
+typedef Suggestion = Tuple3<int, ActionParameter, ActionTemplate>;
 
 /// Form for an action
 class ActionForm extends StatefulWidget {
@@ -48,13 +52,11 @@ class ActionForm extends StatefulWidget {
 }
 
 class _ActionFormState extends State<ActionForm> {
-  final _formKey = GlobalKey<FormState>();
-  Map<String, String> formValues = {};
+  final _formKey = GlobalKey<FormBuilderState>();
 
-  List getSuggestions(String pattern, ActionCatalogueProvider catalogue) {
-    List<Tuple3<int, ActionParameter, ActionTemplate>> suggestions = [];
+  List<Suggestion> getSuggestions(String pattern, ActionCatalogueProvider catalogue) {
+    List<Suggestion> suggestions = [];
     if (pattern.endsWith("#") == false) return suggestions;
-    print(widget.candidate.runtimeType);
     if (widget.candidate is Trigger) return suggestions;
     if (widget.triggerCandidate != null) {
       Trigger trigger = widget.triggerCandidate!;
@@ -62,16 +64,18 @@ class _ActionFormState extends State<ActionForm> {
         (element) => element.name == trigger.name
       );
       for (var parameter in triggerTemplate.returnedValues) {
-        suggestions.add(Tuple3(0, parameter, triggerTemplate));
+        suggestions.add(Suggestion(0, parameter, triggerTemplate));
       }
     }
     int index = 1;
+    int indexOfCandidate = widget.reactionsCandidates.indexOf(widget.candidate as Reaction);
     for (var reactionCandidate in widget.reactionsCandidates) {
-      var reactionTemplate = catalogue.triggerTemplates[reactionCandidate.service]!.firstWhere(
+      if (index == indexOfCandidate + 1) break;
+      var reactionTemplate = catalogue.reactionTemplates[reactionCandidate.service]!.firstWhere(
         (element) => element.name == reactionCandidate.name
       );
       for (var parameter in reactionTemplate.returnedValues) {
-        suggestions.add(Tuple3(index, parameter, reactionTemplate));
+        suggestions.add(Suggestion(index, parameter, reactionTemplate));
       }
       index++;
     }
@@ -83,59 +87,80 @@ class _ActionFormState extends State<ActionForm> {
   @override
   Widget build(BuildContext context) {
     return Consumer<ActionCatalogueProvider>(
-      builder: (__, catalogue, _) => Form(
+      builder: (__, catalogue, _) => FormBuilder(
       key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(widget.description, textAlign: TextAlign.left, style: TextStyle(color: Theme.of(context).colorScheme.onSurface)),
           ...widget.parameters.map((param) {
-            return TypeAheadFormField(
-              initialValue: formValues[param.name] ?? param.value?.toString(),
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.required(context),
-              ]),
-              hideOnEmpty: true,
-              autoFlipDirection: true,
-              textFieldConfiguration: TextFieldConfiguration(
-                decoration: InputDecoration(
-                  labelText: ReCase(param.name).titleCase,
-                  helperText: param.description
-                ),
+            return Autocomplete<Suggestion>(
+              initialValue: TextEditingValue(text: param.value?.toString() ?? ''),
+              fieldViewBuilder: (context, textEditingController, focusNode, onSubmit) => 
+                FormBuilderTextField(
+                  name: param.name,
+                  controller: textEditingController,
+                  focusNode: focusNode,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(context),
+                  ]),
+                  decoration: InputDecoration(
+                    labelText: ReCase(param.name).titleCase,
+                    helperText: param.description
+                  ),
+
               ),
-              suggestionsCallback: (suggestion) => getSuggestions(suggestion, catalogue),
-              noItemsFoundBuilder: (_) => Container(),
-              onSuggestionSelected: (suggestion) {
-                var parameterSuggestion = suggestion as Tuple3<int, ActionParameter, ActionTemplate>;
-                String content = formValues[param.name]!;
-                content += "{${parameterSuggestion.item2}@${parameterSuggestion.item1}}";
+              optionsBuilder: (suggestion) => getSuggestions(suggestion.text, catalogue),
+              // onSelected: (suggestion) {
+              //   var parameterSuggestion = suggestion;
+              //   String content = _formKey.currentState!.value[param.name];
+              //   print(content);
+              //   content = "{${parameterSuggestion.item2}@${parameterSuggestion.item1}}";
+              //   print(content);
+              // },
+              displayStringForOption: (suggestion) {
+                var parameterSuggestion = suggestion;
+                String content = _formKey.currentState!.value[param.name];
                 print(content);
-                setState(() {
-                  formValues[param.name] = content;
-                });
+                content = "{${parameterSuggestion.item2}@${parameterSuggestion.item1}}";
+                print(content);
+                return content;
               },
-              itemBuilder: (context, input) {
-                var suggestion = input as Tuple3<int, ActionParameter, ActionTemplate>;
-                return ListTile(
-                  isThreeLine: true,
-                  dense: true,
-                  leading: suggestion.item3.service.getLogo(logoSize: 30),
-                  title: Text(suggestion.item2.name),
-                  subtitle: Text("${suggestion.item2.description}, from '${suggestion.item3.displayName()}'")
-                );
-              },
-              onSaved: (value) => setState(() {
-                formValues[param.name] = value!;
-              }),
-            );
-          }),
+              optionsViewBuilder: (context, onSelected, inputs) => Align(
+                alignment: Alignment.topLeft,
+                child: Material(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: SizedBox(
+                    height: 60.0 * inputs.length,
+                    width: MediaQuery. of(context). size. width * 0.7, // <-- Right here !
+                    child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      itemCount: inputs.length,
+                      shrinkWrap: false,
+                      itemBuilder: (BuildContext context, int index) {
+                        Suggestion suggestion = inputs.elementAt(index);
+                        return ListTile(
+                          onTap: () => onSelected(suggestion),
+                          isThreeLine: true,
+                          dense: true,
+                          leading: suggestion.item3.service.getLogo(logoSize: 30),
+                          title: Text(suggestion.item2.name),
+                          subtitle: Text("${suggestion.item2.description}, from '${suggestion.item3.displayName()}'")
+                        );
+                      }
+                    ),
+                  )
+                )
+            ));}),
           ...[
             ElevatedButton(
               child: Text(AppLocalizations.of(context).save),
               onPressed: () {
+                _formKey.currentState!.save();
                 if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  widget.onValidate(formValues.map((key, value) => MapEntry(key, value)));
+                  widget.onValidate(_formKey.currentState!.value.map((key, value) => MapEntry(key, value)));
                 }
               },
             ),
