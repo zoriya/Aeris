c9b9c89aeef3c309330c196eb57db2eed295d442
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-03-05 23:24:11 +0100

    intergrating lastTrigger pipeline greatly

--- web-app/src/components/Pipelines/PipelineSquare.tsx
+++ web-app/src/components/Pipelines/PipelineSquare.tsx
@@ -19,6 +19,8 @@ import CableIcon from "@mui/icons-material/Cable";
 import { useTranslation } from "react-i18next";
 import "./PipelineSquare.css";
 import moment from "moment";
+import "moment/locale/fr";
+import i18next from "i18next";
 
 export interface PipelineSquareProps {
 	pipelineData: AppPipelineType;
@@ -55,66 +57,7 @@ export const PipelineSquare = ({ pipelineData, onClick }: PipelineSquareProps) =
 	const errorMode: boolean = pipelineData.data.alertLevel === AlertLevel.Error;
 	const backgroundColor = pipelineData.data.enabled ? (errorMode ? "#ffdddd" : null) : "black";
 	const textColor = pipelineData.data.enabled ? (errorMode ? "red" : null) : "#adadad";
-	moment.locale("fr", {
-		months: "janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre".split("_"),
-		monthsShort: "janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.".split("_"),
-		monthsParseExact: true,
-		weekdays: "dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi".split("_"),
-		weekdaysShort: "dim._lun._mar._mer._jeu._ven._sam.".split("_"),
-		weekdaysMin: "Di_Lu_Ma_Me_Je_Ve_Sa".split("_"),
-		weekdaysParseExact: true,
-		longDateFormat: {
-			LT: "HH:mm",
-			LTS: "HH:mm:ss",
-			L: "DD/MM/YYYY",
-			LL: "D MMMM YYYY",
-			LLL: "D MMMM YYYY HH:mm",
-			LLLL: "dddd D MMMM YYYY HH:mm",
-		},
-		calendar: {
-			sameDay: "[Aujourd’hui à] LT",
-			nextDay: "[Demain à] LT",
-			nextWeek: "dddd [à] LT",
-			lastDay: "[Hier à] LT",
-			lastWeek: "dddd [dernier à] LT",
-			sameElse: "L",
-		},
-		relativeTime: {
-			future: "dans %s",
-			past: "il y a %s",
-			s: "quelques secondes",
-			m: "une minute",
-			mm: "%d minutes",
-			h: "une heure",
-			hh: "%d heures",
-			d: "un jour",
-			dd: "%d jours",
-			M: "un mois",
-			MM: "%d mois",
-			y: "un an",
-			yy: "%d ans",
-		},
-		dayOfMonthOrdinalParse: /\d{1,2}(er|e)/,
-		ordinal: function (number) {
-			return number + (number === 1 ? "er" : "e");
-		},
-		meridiemParse: /PD|MD/,
-		isPM: function (input) {
-			return input.charAt(0) === "M";
-		},
-		// In case the meridiem units are not separated around 12, then implement
-		// this function (look at locale/id.js for an example).
-		// meridiemHour : function (hour, meridiem) {
-		//     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
-		// },
-		meridiem: function (hours, minutes, isLower) {
-			return hours < 12 ? "PD" : "MD";
-		},
-		week: {
-			dow: 1, // Monday is the first day of the week.
-			doy: 4, // Used to determine first week of the year.
-		},
-	});
+	moment.locale(i18next.resolvedLanguage);
 	return (
 		<Card
 			sx={{
@@ -217,7 +160,7 @@ export const PipelineSquare = ({ pipelineData, onClick }: PipelineSquareProps) =
 								<Typography align="left" variant="subtitle2">
 									{pipelineData.data.lastTrigger !== undefined
 										? moment(pipelineData.data.lastTrigger).fromNow()
-										: "not yet triggered"}
+										: t("pipeline_no_last_trigger")}
 								</Typography>
 							</Box>
 							<Box sx={{ display: "flex", flexFlow: "row", alignItems: "center" }}>
