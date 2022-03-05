b880029d3d7a5406512bf6a3b1c52f387e6a66e9
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-05 10:15:27 +0100

    Mobile Client Documentation: update aeris.dart + gen translations on doc gen

--- mobile/lib/aeris.dart
+++ mobile/lib/aeris.dart
@@ -1,9 +1,5 @@
 library aeris;
 
-export 'package:aeris/src/views/startup_page.dart';
-export 'package:aeris/src/widgets/overlayed_texts.dart';
-export 'package:aeris/src/widgets/background/anim_painter.dart';
-export 'package:aeris/src/widgets/background/custom_animation.dart';
 export "package:aeris/src/aeris_api.dart";
 export "package:aeris/src/providers/action_catalogue_provider.dart";
 export "package:aeris/src/providers/pipelines_provider.dart";
